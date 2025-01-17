3ac29c20473dc50a22a0fe19d04dca55deb18ec8
meesooqa <meesooqa@hotmail.com>
2025-01-17 11:22:19 +0300

    output path create

--- cmd/tag/main.go
+++ cmd/tag/main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-	"github.com/meesooqa/tg-stat-tag/internal/format"
 	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/meesooqa/tg-stat-tag/internal/format"
 	"github.com/meesooqa/tg-stat-tag/internal/tag"
 )
 
 func main() {
-	path := os.Args[1]
+	outputBaseDir := "var/output"
+
+	inputPath := os.Args[1]
+	outputPath := createOutputPath(outputBaseDir, inputPath)
 
 	collector := tag.NewTagFileCollector("div.text a")
 	tagService := tag.NewService(collector)
 
-	items := tagService.GetStat(path)
+	items := tagService.GetStat(inputPath)
 
-	f := format.NewHtmlFileFormatter("var/output/sdafsa.html")
+	f := format.NewHtmlFileFormatter(outputPath)
 	f.Format(items)
+}
+
+func createOutputPath(baseDir string, inputPath string) string {
+	outputPath := getOutputPath(baseDir, inputPath)
+
+	dir := filepath.Dir(outputPath)
+	os.MkdirAll(dir, os.ModePerm)
+
+	return outputPath
+}
+
+func getOutputPath(baseDir string, inputPath string) string {
+	cleanPath := filepath.Clean(inputPath)
+	ext := filepath.Ext(cleanPath)
+	baseName := strings.TrimSuffix(cleanPath, ext)
+
+	return filepath.Join(baseDir, baseName+".html")
 }
---
+++ cmd/tag/main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestGetOutputPath(t *testing.T) {
+	baseDir, err := os.MkdirTemp("", "test_html_files")
+	if err != nil {
+		t.Fatalf("Failed to create temporary directory: %v", err)
+	}
+	defer os.RemoveAll(baseDir)
+
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"file.txt", filepath.Join(baseDir, "file.html")},
+		{"dir/file.md", filepath.Join(baseDir, "dir/file.html")},
+		{"folder/subfolder/file", filepath.Join(baseDir, "folder/subfolder/file.html")},
+		{"folder/file.with.multiple.dots", filepath.Join(baseDir, "folder/file.with.multiple.html")},
+		{"/absolute/path/to/file", filepath.Join(baseDir, "absolute/path/to/file.html")},
+		{"relative/path/to/folder/", filepath.Join(baseDir, "relative/path/to/folder.html")},
+		{"simplefile", filepath.Join(baseDir, "simplefile.html")},
+	}
+
+	for _, test := range tests {
+		result := getOutputPath(baseDir, test.input)
+		if result != test.expected {
+			t.Errorf("For input '%s', expected '%s' but got '%s'", test.input, test.expected, result)
+		}
+	}
+}
+
+func TestCreateOutputPath(t *testing.T) {
+	baseDir, err := os.MkdirTemp("", "test_html_files")
+	if err != nil {
+		t.Fatalf("Failed to create temporary directory: %v", err)
+	}
+	defer os.RemoveAll(baseDir)
+
+	tests := []struct {
+		inputPath string
+	}{
+		{filepath.Join(baseDir, "subdir", "file.html")},
+		{filepath.Join(baseDir, "deeply", "nested", "folder", "file.html")},
+		{filepath.Join(baseDir, "anotherdir", "file.html")},
+	}
+
+	for _, test := range tests {
+		createOutputPath("", test.inputPath)
+
+		dir := filepath.Dir(test.inputPath)
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			t.Errorf("Directory '%s' was not created", dir)
+		}
+	}
+}
